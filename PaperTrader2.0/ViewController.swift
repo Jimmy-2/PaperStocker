aaeea9d6b4d9a912a402993e5c8438592716ad22
Jimmy-2 <jwu4477@gmail.com>
2021-11-19 21:43:15 -0500

    Initial Commit

---
+++ PaperTrader2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaperTrader2.0
+//
+//  Created by Jimmy  on 11/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
